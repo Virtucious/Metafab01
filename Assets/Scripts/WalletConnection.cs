bf27dbb5680c235d030ca64c48e8c73ef7ced3ce
Virtucious <aman.rahman.0309@gmail.com>
2023-01-05 23:08:23 +0530

    Added WalletConnection

--- Assets/Scripts/WalletConnection.cs
+++ Assets/Scripts/WalletConnection.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MetafabSdk;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Linq;
 
 public class WalletConnection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public Chain chain = Chain.MATICMUMBAI;
 
-    // Update is called once per frame
-    void Update()
+	public string playerUsername = "sample-player";
+	public string playerPassword = "password";
+
+	public string currencyName = "My Test Coin";
+	public string currencySymbol = "$MYTEST";
+
+	public async void GetWallet()
     {
-        
-    }
+		Metafab.SecretKey = "game_sk_Xn0Z3fU3oKZZhcvPkGWZJs158rvWq3hxzo/YdsuPGDfsl7r4HadjQKR7orATGzwr";
+		Metafab.Password = "Virtucious@101";
+		Debug.Log($"Getting player...");
+		var players = await Metafab.PlayersApi.GetPlayers();
+		var player = players[players.Count - 1];
+		Debug.Log($"Got player {player}.");
+	}
+
+
+	
 }
