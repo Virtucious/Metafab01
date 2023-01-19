f03d3acbe9b7d7b6f82c0894e75adfc80357034b
Virtucious <aman.rahman.0309@gmail.com>
2023-01-19 22:56:00 +0530

    added jump animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,31 +3,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float playerSpeed = 5.0f;
-    [SerializeField] private float jumpPower = 1f;
 
     private Rigidbody2D _playerRigidbody;
-    private Animator animator;
+    public Animator animator;
+
     private void Start()
-    {
-        animator = GetComponent<Animator>();
+    { 
         _playerRigidbody = GetComponent<Rigidbody2D>();
         if (_playerRigidbody == null)
         {
             Debug.LogError("Player is missing a Rigidbody2D component");
         }
     }
+
     private void Update()
     {
         MovePlayer();
     }
+
     private void MovePlayer()
     {
-        animator.SetBool("isGrounded", true);
+        var ground = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
+        Debug.Log(_playerRigidbody.velocity.y);
+        if (_playerRigidbody.velocity.y == 0)
+        {
+            animator.SetBool("isFalling", false);
+            animator.SetBool("isJumping", false);
+        }
+        if (_playerRigidbody.velocity.y > 0)
+        {
+            animator.SetBool("isJumping", true);
+        }
+        if (_playerRigidbody.velocity.y < 100)
+        {
+            animator.SetBool("isFalling", true);
+        }
         if (Input.GetKey(KeyCode.A))
         {
             Vector3 currentScale = gameObject.transform.localScale;
             currentScale.x = -1;
-            gameObject.transform.localScale = currentScale; 
+            gameObject.transform.localScale = currentScale;
             _playerRigidbody.velocity = new Vector2(-1 * playerSpeed, _playerRigidbody.velocity.y);
             animator.SetBool("Move", true);
         }
@@ -39,28 +54,12 @@ public class PlayerMovement : MonoBehaviour
             _playerRigidbody.velocity = new Vector2(1 * playerSpeed, _playerRigidbody.velocity.y);
             animator.SetBool("Move", true);
         }
-        else if (Input.GetKeyDown(KeyCode.Space))
+        else if (Input.GetKey(KeyCode.Space))
         {
-            var ground = Physics2D.Raycast(transform.position, Vector2.down, 0.3f);
-            _playerRigidbody.AddForce(transform.up * jumpPower);
             if (ground.collider != null)
             {
-                animator.SetBool("isGrounded", false);
-
+                _playerRigidbody.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
             }
-            else
-            {
-                animator.SetBool("isGrounded", true);
-            }
-
-
-        }
-        else
-        { 
-            animator.SetBool("Move", false);
         }
     }
-
-
-
 }
